a21b445bbe540fbdcd8ad15cd55caa4011c23f7f
Marzio Cassese <marzio.cassese@edu.tbz.ch>
2022-06-03 13:00:55 +0200

    New Game Update

--- New Game/index.js
+++ New Game/index.js
@@ -3,13 +3,13 @@ const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
-const gravity = 0.5
+const gravity = 1
 
 class Player {
     constructor() {
         this.position = {
-            x: 100,
-            y: 100
+            x: 230,
+            y: 10
         }
         this.velocity = {
             x:0,
@@ -34,7 +34,22 @@ class Player {
     }
 }
 
+class Plattform {
+    constructor() {
+        this.position = {
+            x: 200,
+            y: 300
+        }
+        this.width = 190
+        this.height = 30
+    }
+    draw() {
+        c.fillStyle = "purple"
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
 const player = new Player()
+const plattform = new Plattform()
 const keys = {
     rechts: {
         pressed: false
@@ -50,12 +65,21 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+    plattform.draw()
 
     if(keys.rechts.pressed) {
         player.velocity.x = 5
     }else if (keys.links.pressed) {
         player.velocity.x = -5
     }else player.velocity.x = 0
+
+    //plattform collision detection
+    if (player.position.y + player.height <= plattform.position.y
+        && player.position.y + player.height + player.velocity.y >= plattform.position.y
+        && player.position.x + player.width >= plattform.position.x
+        && player.position.x <= plattform.position.x + plattform.width) {// && = add statement (conditional)
+        player.velocity.y = 0
+    }
 }
 animate()
 
@@ -67,7 +91,7 @@ addEventListener('keydown', ({ keyCode }) => {
             break
         case 38:
             console.log('oben')
-            player.velocity.y -= 10
+            player.velocity.y -= 7
             break
         case 39:
             console.log('rechts')
