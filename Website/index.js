238d8f3eb6dc1316b94a6ed5a6089a8e5ff5d9cd
Marzio Cassese <marzio.cassese@edu.tbz.ch>
2022-06-10 15:08:47 +0200

    Website

--- Website/index.js
+++ Website/index.js
@@ -1,6 +1,7 @@
-const responsiveButton = document.getElementsByClassName('responsive-button') [0]
-const links = document.getElementsByClassName('links') [0]
+const responsiveButton =
+document.getElementsByClassName("responsive-button")[0];
+const links = document.getElementsByClassName("links")[0];
 
-responsiveButton.addEventListener('click', () => {
-    links.classList.toggle('active')
-})
+responsiveButton.addEventListener("click", () => {
+links.classList.toggle("active");
+});
