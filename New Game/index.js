513d0716cc8b212f9c3c353ff5575fa7c30f2170
Marzio Cassese <marzio.cassese@edu.tbz.ch>
2022-06-10 15:09:10 +0200

    Jump and Run Game

--- New Game/index.js
+++ New Game/index.js
@@ -1,146 +1,156 @@
-
 class Player {
-    constructor() {
-        this.position = {
-            x: 90,
-            y: 10
-        }
-        this.tempo = {
-            x: 0,
-            y: 0
-        }
-        this.width = 45
-        this.height = 45
-    }
-    draw() { //draw function
-        c.fillStyle = "red"
-        c.fillRect(this.position.x, this.position.y,
-            this.width, this.height)
-    }
-    update() { //update function
-        this.draw()
-        this.position.x += this.tempo.x
-        this.position.y += this.tempo.y
+constructor() {
+    this.position = {
+    x: 90,
+    y: 10,
+    };
+    this.tempo = {
+    x: 0,
+    y: 0,
+    };
+    this.width = 45;
+    this.height = 45;
+}
+draw() {
+    //draw function
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+}
+update() {
+    //update function
+    this.draw();
+    this.position.x += this.tempo.x;
+    this.position.y += this.tempo.y;
 
-        if (this.position.y + this.height + this.tempo.y <= canvas.height)
-            this.tempo.y += gravity
+    if (this.position.y + this.height + this.tempo.y <= canvas.height)
+        this.tempo.y += gravity;
     }
 }
 
 class Plattform {
     constructor({ x, y }) {
-        this.position = {
-            x: x,
-            y: y
-        }
-        this.width = 190
-        this.height = 30
+    this.position = {
+        x: x,
+        y: y,
+    };
+    this.width = 190;
+    this.height = 30;
     }
     draw() {
-        c.fillStyle = "yellow"
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    c.fillStyle = "yellow";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 }
 
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width = innerWidth
-canvas.height = innerHeight
-const gravity = 0.8
-
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+const gravity = 0.8;
 
 const levels = [
     [
-        new Plattform({ x: 90, y: 500 }),
-        new Plattform({ x: 400, y: 500 }),
-        new Plattform({ x: 700, y: 500 })
-
+    new Plattform({ x: 90, y: 500 }),
+    new Plattform({ x: 400, y: 500 }),
+    new Plattform({ x: 700, y: 500 }),
     ],
     [
-        new Plattform({ x: 90, y: 300 }),
-        new Plattform({ x: 450, y: 300 }),
-        new Plattform({ x: 800, y: 300 }),
-        new Plattform({ x: 1120, y: 300 })
+    new Plattform({ x: 90, y: 300 }),
+    new Plattform({ x: 450, y: 300 }),
+    new Plattform({ x: 800, y: 300 }),
+    new Plattform({ x: 1120, y: 300 }),
     ],
     [
-        new Plattform({ x: 90, y: 300 }),
-        new Plattform({ x: 450, y: 300 }),
-        new Plattform({ x: 800, y: 480 }),
-        new Plattform({ x: 1020, y: 380 }),
-        new Plattform({ x: 1250, y: 450 })
+    new Plattform({ x: 90, y: 300 }),
+    new Plattform({ x: 450, y: 300 }),
+    new Plattform({ x: 800, y: 480 }),
+    new Plattform({ x: 1020, y: 380 }),
+    new Plattform({ x: 1250, y: 450 }),
     ],
     [
-        new Plattform({ x: 90, y: 500 }),
-        new Plattform({ x: 500, y: 400 }),
-        new Plattform({ x: 800, y: 280 }),
-        new Plattform({ x: 1120, y: 210 }),
-        new Plattform({ x: 1420, y: 150 }),
-        new Plattform({ x: 1820, y: 450 })
+    new Plattform({ x: 90, y: 500 }),
+    new Plattform({ x: 500, y: 400 }),
+    new Plattform({ x: 800, y: 280 }),
+    new Plattform({ x: 1120, y: 210 }),
+    new Plattform({ x: 1420, y: 150 }),
+    new Plattform({ x: 1820, y: 450 }),
     ],
     [
-        new Plattform({ x: 90, y: 500 }),
-        new Plattform({ x: 500, y: 400 }),
-        new Plattform({ x: 800, y: 280 }),
-        new Plattform({ x: 1120, y: 280 }),
-        new Plattform({ x: 1520, y: 500 }),
-        new Plattform({ x: 1720, y: 400 }),
-        new Plattform({ x: 1900, y: 280 }),
-        new Plattform({ x: 2200, y: 200 }),
-        new Plattform({ x: 2600, y: 450 })
+    new Plattform({ x: 90, y: 500 }),
+    new Plattform({ x: 500, y: 400 }),
+    new Plattform({ x: 800, y: 280 }),
+    new Plattform({ x: 1120, y: 280 }),
+    new Plattform({ x: 1520, y: 500 }),
+    new Plattform({ x: 1720, y: 400 }),
+    new Plattform({ x: 1900, y: 280 }),
+    new Plattform({ x: 2200, y: 200 }),
+    new Plattform({ x: 2600, y: 450 }),
     ],
     [
-        new Plattform({ x: 90, y: 100 }),
-        new Plattform({ x: 500, y: 200 }),
-        new Plattform({ x: 800, y: 300 }),
-        new Plattform({ x: 1120, y: 480 }),
-        new Plattform({ x: 1520, y: 480 }),
-        new Plattform({ x: 1720, y: 350 }),
-        new Plattform({ x: 1900, y: 280 }),
-        new Plattform({ x: 2200, y: 200 }),
-        new Plattform({ x: 2400, y: 160 })
+    new Plattform({ x: 90, y: 100 }),
+    new Plattform({ x: 500, y: 200 }),
+    new Plattform({ x: 800, y: 300 }),
+    new Plattform({ x: 1120, y: 480 }),
+    new Plattform({ x: 1520, y: 480 }),
+    new Plattform({ x: 1720, y: 350 }),
+    new Plattform({ x: 1900, y: 280 }),
+    new Plattform({ x: 2200, y: 200 }),
+    new Plattform({ x: 2400, y: 160 }),
     ],
     [
-        new Plattform({ x: 90, y: 540 }),
-        new Plattform({ x: 400, y: 450 }),
-        new Plattform({ x: 700, y: 400 }),
-        new Plattform({ x: 900, y: 350 }),
-        new Plattform({ x: 960, y: 350 }),
-        new Plattform({ x: 1000, y: 350 }),
-        new Plattform({ x: 1100, y: 350 }),
-        new Plattform({ x: 1400, y: 300 }),
-        new Plattform({ x: 1600, y: 500 })
+    new Plattform({ x: 90, y: 540 }),
+    new Plattform({ x: 400, y: 450 }),
+    new Plattform({ x: 700, y: 400 }),
+    new Plattform({ x: 900, y: 350 }),
+    new Plattform({ x: 960, y: 350 }),
+    new Plattform({ x: 1000, y: 350 }),
+    new Plattform({ x: 1100, y: 350 }),
+    new Plattform({ x: 1400, y: 300 }),
+    new Plattform({ x: 1600, y: 500 }),
     ],
     [
-        new Plattform({ x: 90, y: 300 }),
-        new Plattform({ x: 450, y: 400 }),
-        new Plattform({ x: 800, y: 300 }),
-        new Plattform({ x: 1120, y: 400 }),
-        new Plattform({ x: 1350, y: 300 }),
-        new Plattform({ x: 1520, y: 300 })
+    new Plattform({ x: 90, y: 300 }),
+    new Plattform({ x: 450, y: 400 }),
+    new Plattform({ x: 800, y: 300 }),
+    new Plattform({ x: 1120, y: 400 }),
+    new Plattform({ x: 1350, y: 300 }),
+    new Plattform({ x: 1520, y: 300 }),
     ],
     [
-        new Plattform({ x: 90, y: 500 }),
-        new Plattform({ x: 450, y: 500 }),
-        new Plattform({ x: 800, y: 450 }),
-        new Plattform({ x: 1020, y: 380 }),
-        new Plattform({ x: 1250, y: 250 }),
-        new Plattform({ x: 1700, y: 450 }),
-        new Plattform({ x: 1500, y: 350 }),
-        new Plattform({ x: 1250, y: 480 })
+    new Plattform({ x: 90, y: 500 }),
+    new Plattform({ x: 450, y: 500 }),
+    new Plattform({ x: 800, y: 450 }),
+    new Plattform({ x: 1020, y: 380 }),
+    new Plattform({ x: 1250, y: 250 }),
+    new Plattform({ x: 1700, y: 450 }),
+    new Plattform({ x: 1500, y: 350 }),
+    new Plattform({ x: 1250, y: 480 }),
     ],
     [
-        new Plattform({ x: 90, y: 100 }),
-        new Plattform({ x: 280, y: 130 }),
-        new Plattform({ x: 470, y: 160 }),
-        new Plattform({ x: 660, y: 190 }),
-        new Plattform({ x: 470, y:  450  }),
-        new Plattform({ x: 930, y: 290 }),
-        new Plattform({ x: 740, y: 320 }),
-        new Plattform({ x:1120, y: 260}),
-    ]
-]
-
+    new Plattform({ x: 90, y: 100 }),
+    new Plattform({ x: 280, y: 130 }),
+    new Plattform({ x: 470, y: 160 }),
+    new Plattform({ x: 660, y: 190 }),
+    new Plattform({ x: 470, y: 450 }),
+    new Plattform({ x: 930, y: 290 }),
+    new Plattform({ x: 740, y: 320 }),
+    new Plattform({ x: 1120, y: 260 }),
+    ],
+    [
+    new Plattform({ x: 30, y: 210 }),
+    new Plattform({ x: 750, y: 160 }),
+    new Plattform({ x: 550, y: 280 }),
+    new Plattform({ x: 360, y: 450 }),
+    new Plattform({ x: 720, y: 380 }),
+    new Plattform({ x: 920, y: 500 }),
+    new Plattform({ x: 1120, y: 400 }),
+    new Plattform({ x: 1300, y: 280 }),
+    new Plattform({ x: 1000, y: 200 }),
+    new Plattform({ x: 440, y: 140 }),
+    new Plattform({ x: 90, y: 400 }),
+    ],
+];
 
 let currentLevel = 0;
 
@@ -148,123 +158,119 @@ let platforms = levels[currentLevel];
 let player = new Player();
 let win = false;
 
-
 const keys = {
     rechts: {
-        pressed: false
+    pressed: false,
     },
     links: {
-        pressed: false
+    pressed: false,
     },
-}
+};
 
-let scrollOffset = 0
+let scrollOffset = 0;
 
 function initializePlayer() {
-    player = new Player()
-    scrollOffset = 0
+    player = new Player();
+    scrollOffset = 0;
 }
 
-function champion()
-{
-    alert("Level erfolgreich absolviert! Ab zum nächsten!")
-    currentLevel++;
-    platforms = levels[currentLevel];
-    win = false;
+function champion() {
+alert("Level erfolgreich absolviert! Ab zum nächsten!");
+currentLevel++;
+platforms = levels[currentLevel];
+win = false;
 
-    if(platforms) {
-
-        initializePlayer()
+    if (platforms) {
+    initializePlayer();
     } else {
-        window.location.href="./Erklärung/index.html"
+    window.location.href = "./Erklärung/index.html";
     }
-
 }
 function animate() {
-    if (win) champion()
+    if (win) champion();
 
-    requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
-    player.update()
-    platforms.forEach(plattform => {
-        plattform.draw()
-    })
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    platforms.forEach((plattform) => {
+    plattform.draw();
+    });
 
-    console.log(player)
+    console.log(player);
 
-    if (keys.rechts.pressed && player.position.x < 500) {
-        player.tempo.x = 5
-    } else if (keys.links.pressed && player.position.x > 100) {
-        player.tempo.x = -5
+    if (keys.rechts.pressed && player.position.x < 800) {
+    player.tempo.x = 5;
+    } else if (keys.links.pressed && player.position.x > 200) {
+    player.tempo.x = 5;
     } else {
-        player.tempo.x = 0
+    player.tempo.x = 0;
 
-        if (keys.rechts.pressed) {
-            scrollOffset += 5
-            platforms.forEach(plattform => {
-                plattform.position.x -= 5
-            })
-
-        } else if (keys.links.pressed) {
-            scrollOffset -= 5
-            platforms.forEach(plattform => {
-                plattform.position.x += 5
-            })
-        }
+    if (keys.rechts.pressed) {
+        scrollOffset += 5;
+        platforms.forEach((plattform) => {
+        plattform.position.x -= 5;
+        });
+    } else if (keys.links.pressed) {
+        scrollOffset -= 5;
+        platforms.forEach((plattform) => {
+        plattform.position.x += 5;
+        });
+    }
     }
 
-    //plattform collision detection
+  //plattform collision detection
 
     platforms.forEach((plattform, i) => {
-        if (player.position.y + player.height <= plattform.position.y
-            && player.position.y + player.height + player.tempo.y >= plattform.position.y
-            && player.position.x + player.width >= plattform.position.x
-            && player.position.x <= plattform.position.x + plattform.width) {// && = add statement (conditional)
-            player.tempo.y = 0
-            if (i + 1 == platforms.length) {
-                win = true;
-            }
+    if (
+        player.position.y + player.height <= plattform.position.y &&
+        player.position.y + player.height + player.tempo.y >=
+        plattform.position.y &&
+        player.position.x + player.width >= plattform.position.x &&
+        player.position.x <= plattform.position.x + plattform.width
+    ) {
+      // && = add statement (conditional)
+        player.tempo.y = 0;
+        if (i + 1 == platforms.length) {
+        win = true;
         }
-    })
-    // condition fürs gewinnen
-    if (scrollOffset > 2200) {
-        console.log('Du hast es geschafft! Ab zum nächsten Level')
     }
-    // condition fürs verlieren
+    });
+  // condition fürs gewinnen
+
+  // condition fürs verlieren
     if (player.position.y > canvas.height) {
-        initializePlayer()
+    initializePlayer();
     }
 }
 
-animate()
+animate();
 
-addEventListener('keydown', ({ keyCode }) => {
+addEventListener("keydown", ({ keyCode }) => {
     switch (keyCode) {
-        case 37:
-            keys.links.pressed = true
-            break
-        case 38:
-            player.tempo.y -= 12
-            break
-        case 39:
-            keys.rechts.pressed = true
-            break
-        case 40:
-            break
-    } //switch case statment
-})
-addEventListener('keyup', ({ keyCode }) => {
+    case 37:
+        keys.links.pressed = true;
+        break;
+    case 38:
+        player.tempo.y -= 12;
+        break;
+    case 39:
+        keys.rechts.pressed = true;
+        break;
+    case 40:
+        break;
+  } //switch case statment
+});
+addEventListener("keyup", ({ keyCode }) => {
     switch (keyCode) {
-        case 37:
-            keys.links.pressed = false
-            break
-        case 38:
-            break
-        case 39:
-            keys.rechts.pressed = false
-            break
-        case 40:
-            break
-    } //switch case statment
-})
-
+    case 37:
+        keys.links.pressed = false;
+        break;
+    case 38:
+        break;
+    case 39:
+        keys.rechts.pressed = false;
+        break;
+    case 40:
+        break;
+  } //switch case statment
+});
